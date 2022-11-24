9b7724695d6f5dffd4449d743146016b88a1c524
agushendra77903 <117900786+agushendra77903@users.noreply.github.com>
2022-11-24 16:14:25 +0700

    Add files via upload

--- 1_D_71220959.py
+++ 1_D_71220959.py
@@ -2,18 +2,18 @@ print("\(^o^)/ selamat datang di kalkulator sederhana \(^o^)/")
 print("masukkan angka yang kalian inginkan")
 print("kemudian jalankan sesuai dengan arahan ")
 
-angka1 = int(input("masukan angka pertama : "))
-angka2 = int(input("masukan angka kedua : "))
+variabel1 = int(input("masukan angka pertama : "))
+variabel2 = int(input("masukan angka kedua : "))
 operator = input("masukan operator(+,*,/,-) : ")
 hasil = 0
 if operator == "+":
-        hasil = angka1+angka2
+        hasil = variabel1+variabel2
 elif operator == "-":
-        hasil = angka1-angka2
+        hasil = variabel1-variabel2
 elif operator == "*":
-        hasil = angka1*angka2
+        hasil = variabel1*variabel2
 elif operator == "/":
-        hasil = angka1/angka2
+        hasil = variabel1/variabel2
 else:
         print("operator tak diketahui")
 
